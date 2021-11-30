7de819b3310e6f2770084bbe82b827422fa4c5dd
ScottAnderson19 <93892791+ScottAnderson19@users.noreply.github.com>
2021-11-30 16:13:15 +1300

    media and nav

--- src/js/app.js
+++ src/js/app.js
@@ -10,18 +10,41 @@ if (window.netlifyIdentity) {
 }
 
 
+//
+// function hamburgerMenu() {
+//   var x = document.getElementById("Hamburger");
+//   if (x.className === "hamburger") {
+//     x.className += " active";
+//   } else {
+//     x.className = "hamburger";
+//   }
+//   var x = document.getElementById("nav");
+//   if (x.className === "menu") {
+//     x.className += " active";
+//   } else {
+//     x.className = "menu";
+//   }
+// }
+$(document).ready(function() {
 
-function hamburgerMenu() {
-  var x = document.getElementById("Hamburger");
-  if (x.className === "hamburger") {
-    x.className += " active";
-  } else {
-    x.className = "hamburger";
-  }
-  var x = document.getElementById("nav");
-  if (x.className === "menu") {
-    x.className += " active";
-  } else {
-    x.className = "menu";
-  }
-}
+  $(".hamburger").click(function() {
+    $(".overlay").fadeToggle(200);
+  });
+});
+
+$('.overlay').on('click', function() {
+  $(".overlay").fadeToggle(200);
+
+  open = false;
+});
+
+var $hamburger = $(".hamburger");
+$hamburger.on("click", function(e) {
+  $hamburger.toggleClass("is-active"), $(".overlay").fadeToggle(200);
+  // Do something else, like open / close menu
+});
+var $hamburger = $(".hamburger");
+$hamburger.on("click", function(e) {
+  $hamburger.removeclass("is-active");
+
+});
