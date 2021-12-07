a08a6c1e55c6acabd1ac7023f622b28559d796bc
ScottAnderson19 <93892791+ScottAnderson19@users.noreply.github.com>
2021-12-07 15:57:20 +1300

    js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,5 @@
 
 import "./css/main.css";
 
-
-// HAMBURGER
-
-$(document).ready(function(){
-  $(".hamburger").click(function(){
-    $(this).toggleClass("active");
-    $('.mobile-nav').toggleClass("active");
-  });
-});
-
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
